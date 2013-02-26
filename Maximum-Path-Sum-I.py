5eed2094df50a77cb16f00a6600a180b3630d6ab
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-02-25 23:08:53 -0500

    Program finds fastest path

--- Maximum-Path-Sum-I.py
+++ Maximum-Path-Sum-I.py
@@ -11,30 +11,51 @@ def binaryMap(toBinary):
     binaryList = []
     tempString = ""
     tempString = str(bin(toBinary))
+    for i in range(6 - len(tempString)):
+        binaryList.append('0')
     for x in range(2, len(tempString)):
         binaryList.append(tempString[x])
     return binaryList
-
 
 def bottomUp(rowCheck, columnCheck): 
     total = 0
     tempTotal = 0
     highestList = []
     tempList = []
- 
-    while True: 
-        for moveUp in range(rowCheck, (rowCheck - 5), -1): 
-            for moveHoriz in mapper: # Working out how to itirate through each series - maybe another nested loop...
-                tempList.append(numList[moveUp][moveHoriz])
-                
+    mapperCount = 0
+    mapper = []
+    binMap = []
+    counter = 0
+
+    for i in range(16):
+        currentColumn = columnCheck
+        currentRow = rowCheck
+        binMap = binaryMap(i) # generates map to move up or left
+        mapper.append(columnCheck)
+        for z in binMap:    
+            currentRow -= 1 # move to row 14 - up
+            currentColumn -= int(z)
+            if(currentColumn >= currentRow):
+                mapper.append(currentRow)
+            elif(currentColumn < 0):
+                mapper.append(0)
+            else:
+                mapper.append(currentColumn)
+        
+        for moveUp in range(rowCheck, (rowCheck - 5), -1): #should not be nested
+            # for moveHoriz in mapper:
+            tempList.append(numList[moveUp][mapper[counter]])
+            counter += 1
+            
         for x in tempList:
-            tempTotal = tempTotal + x   
+            tempTotal += int(x)   
         if(tempTotal > total):
             total = tempTotal
-            partialList = tempList
-        
+            highestList = tempList
         tempList = []
         tempTotal = 0
+        counter = 0
+        mapper = []
     
     return highestList
 
@@ -42,9 +63,6 @@ numList = str(txt.read()).split("\n", 15)
 
 for x in range(15):
     numList[x] = numList[x].split(" ", 15)
-    
-print len(numList[14]) # Returns how many are in row - maybe useful later - maybe easier to use a formula
-
-print bin(1)
-print binaryMap(1)
+# print numList[14][0]  
+print bottomUp(14, 9)
     
